f780788e938d1e6003cc94b079155d079ff399c4
datnguyenbhx <duc.dat@beehexa.com>
2024-11-23 08:53:49 +0700

    add get latest release url

--- data/constants.js
+++ data/constants.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 
 class Constants {
   static CONFIGURATION_FILE_PATH = "data/configuration.json";
+  static GITHUB_RELEASE_URL = "beehexacorp/sql-account-rest-api/releases/";
   static ReadJsonFile(filePath) {
     return new Promise((resolve, reject) => {
       fs.readFile(filePath, "utf8", (err, data) => {
