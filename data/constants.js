87829b8821335b74007af5da72d18170511dd784
datnguyenbhx <duc.dat@beehexa.com>
2024-11-23 10:52:12 +0700

    remove class base + ensure file exist in  ReadJsonFile

--- data/constants.js
+++ data/constants.js
@@ -1,42 +1,56 @@
 const fs = require("fs");
 
-class Constants {
-  static CONFIGURATION_FILE_PATH = "data/configuration.json";
-  static GITHUB_RELEASE_URL = "beehexacorp/sql-account-rest-api/releases/";
-  static ReadJsonFile(filePath) {
-    return new Promise((resolve, reject) => {
-      fs.readFile(filePath, "utf8", (err, data) => {
-        if (err) {
-          reject("Error when reading file:", err);
+export const CONFIGURATION_FILE_PATH = "data/configuration.json";
+export const GITHUB_RELEASE_URL = "beehexacorp/sql-account-rest-api/releases/";
+
+export function ReadJsonFile(filePath) {
+  return new Promise((resolve, reject) => {
+    if (!fs.existsSync(filePath)) {
+      // Create empty file if not exists
+      fs.writeFile(filePath, JSON.stringify({}, null, 2), "utf8", (writeErr) => {
+        if (writeErr) {
+          reject("Error creating file:", writeErr);
           return;
         }
-        try {
-          const jsonData = JSON.parse(data);
-          resolve(jsonData);
-        } catch (parseErr) {
-          reject("Error parsing JSON:", parseErr);
-        }
+        resolve({}); 
       });
+      return;
+    }
+    fs.readFile(filePath, "utf8", (err, data) => {
+      if (err) {
+        reject("Error when reading file:", err);
+        return;
+      }
+      try {
+        const jsonData = JSON.parse(data);
+        resolve(jsonData);
+      } catch (parseErr) {
+        resolve({});
+        reject("Error parsing JSON:", parseErr);
+      }
     });
-  }
-  static WriteJsonFile(filePath, newData) {
-    return new Promise((resolve, reject) => {
-      Constants.ReadJsonFile(filePath)
-        .then((jsonData) => {
-          jsonData = { ...jsonData, ...newData };
-          const jsonString = JSON.stringify(jsonData, null, 2);
-          fs.writeFile(filePath, jsonString, (err) => {
-            if (err) {
-              reject("Error writing file:", err);
-              return;
-            }
-            resolve("File written successfully.");
-          });
-        })
-        .catch((err) => {
-          reject("Error reading JSON file:", err);
-        });
-    });
-  }
+  });
 }
-module.exports = Constants;
+export function WriteJsonFile(filePath, newData) {
+  return new Promise((resolve, reject) => {
+    ReadJsonFile(filePath)
+      .catch((err) => {
+        log("Warning: ReadJsonFile failed. Proceeding with empty object.", err);
+        return {}; 
+      })
+      .then((jsonData) => {
+        jsonData = { ...jsonData, ...newData };
+        const jsonString = JSON.stringify(jsonData, null, 2);
+        fs.writeFile(filePath, jsonString, (err) => {
+          if (err) {
+            reject("Error writing file:", err);
+            return;
+          }
+          resolve("File written successfully.");
+        });
+      })
+      .catch((err) => {
+        reject("Error reading JSON file:", err);
+      });
+  });
+}
